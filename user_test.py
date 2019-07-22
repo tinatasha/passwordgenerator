ff1d94a52c93462d0cf2641a3b2654cd6d04b4b1
tinatasha <tashambiti@gmail.com>
2019-07-22 07:53:10 +0300

    add user details

--- user_test.py
+++ user_test.py
@@ -10,3 +10,13 @@ class TestUser(unittest.TestCase):
         Setup method that defines instructions
         """
         self.new_user = User("Denis","Kibet","soccer5240")
+		
+		def test_init(self):
+        """
+        Test for correct initialization
+        """
+        self.assertEqual(self.new_user.first_name,"Denis")
+        self.assertEqual(self.new_user.last_name,"Kibet")
+        self.assertEqual(self.new_user.password,"soccer5240")
+		
+		
