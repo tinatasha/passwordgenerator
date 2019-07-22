3b33c503ffda6039065c13cca1e197f9d735bd54
tinatasha <tashambiti@gmail.com>
2019-07-22 08:01:57 +0300

    modified changes

--- run.py
+++ run.py
@@ -47,4 +47,86 @@ def save_user(user):
     """
     credential.delete_credentials()
 
+def main():
+    # print("Welcome to password locker :)")
+    # print("What\'s your name?")
+    # name = input()
+	
+	while True:
+
+        print(f"Hello.Please use the short codes :li to create your password locker account :ex - To exit the application")
+    
+        short_code = input().lower()
+
+        if short_code == 'li':
+
+            print('Login')
+            print('*'*15)
+
+            print("First name....")
+            firstname = input()
+
+            print("Last name....")
+            lastname = input()
+
+            print("Password....")
+            password = input()
+
+            save_user(create_login(firstname,lastname,password))
+            print('\n')
+            
+            while True:
+
+                print(f"Welcome {firstname}.Use the following commands: ca-Create a new account :da - Display all accounts :dl - Delete an account :lo - Log out ")
+                print('\n')
+                shorter_code = input().lower()
+
+                if shorter_code == 'ca':
+                    print("Save new account credentials")
+                    print('^'*10)
+
+                    print('Name of the account.....')
+                    accname = input()
+
+                    print('Username.....')
+                    username = input()
+                    while True:
+                        print('Use: \n ep >> To input your password \n gp >> To generate a password')
+                        pass_code = input().lower()
+
+                        if pass_code == 'ep':
+
+                            print('Password....')
+                            password = input()
+                            break
+
+                        elif pass_code == 'gp':
+                            password = generate_password()
+                            break
+
+                        else:
+                            pass
+
+                    save_credential(add_account(accname,username,password))
+                    print('\n')
+                    print(f"New account \'{accname}\' created")
+                        
+
+                elif shorter_code == 'da':
+                    if display_credentials():
+                        for credential in display_credentials():
+                            print('-' * 20)
+                            print(f" Account name >> {credential.account_name}  \n Username >> {credential.username} \n password >> {credential.password}")
+                            print('-' * 20)
+							
+							elif shorter_code == 'dl':
+                    pass
+
+                elif shorter_code == 'lo':
+                    print('Thank you for using this app :)')
+                    break
+
+                else:
+                    print('Sorry :( .The command doesn\'t exist.Try again with :li' )
+                    print('-'*8)
 	
