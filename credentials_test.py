98fda775769882b1eb462ff7583f80878bbe541e
tinatasha <tashambiti@gmail.com>
2019-07-22 08:12:49 +0300

    modified changes

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,18 @@
 import unittest
 from password import Credentials
+
+class TestCredentials(unittest.TestCase):
+    """
+    Class to test behaviour of the credentials class
+    """
+    def setUp(self):
+        """
+        Setup method that defines instructions
+        """
+        self.new_credentials = Credentials("Github","Kibet1816","@#soccerkibe1816")
+
+    def tearDown(self):
+        """
+        Method that cleans up after each test
+        """
+        Credentials.credentials_list = []
